9becc4ab51bd47947788dd8baabcc21047b7fbed
Talen Fisher <talen.fisher@cythral.com>
2020-04-27 12:01:17 -0500

    remove previous attempt at auto scale down & add enable_service_stopping tag

---
+++ deploy/create-config-files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cwd=$(dirname "${BASH_SOURCE[0]}")
+
+for file in $(ls ${cwd}/params/*.json); do
+    envName=$(echo $file | xargs basename | sed "s/\.json//")
+    params=$(cat $file)
+    params=$(echo $params | jq ".Image=\"$1\"")
+    
+    config={}
+    config=$(echo $config | jq --argjson params "$params" '.Parameters=$params')
+    echo $config > cfn-hello-world.${envName}.config.json
+done
